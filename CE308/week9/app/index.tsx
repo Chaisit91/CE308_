bab0f4eef831fe1c99df2872c6e8dcd389226ecd
Chaisit91 <Jorvinbasla@gmail.com>
2026-03-16 11:52:44 +0700

    add

--- CE308/week9/app/index.tsx
+++ CE308/week9/app/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Provider} from 'react-redux';
 import { store } from './redux/store';
-import CounterScreen  from "./screens/CounterScreen";
+import CounterScreen  from "./screens/ShoppingCartScreen";
+import ShoppingCartScreen from "./screens/ShoppingCartScreen";
+import TodoScreen from "./screens/TodoScreen";
+
 
 
 export default function App(){
   return (
     <Provider store={store}>
-      <CounterScreen />
+      <ShoppingCartScreen />
+      <TodoScreen />
     </Provider>
   )
 }
--- CE308/week9/app/redux/store.ts
+++ CE308/week9/app/redux/store.ts
@@ -1,15 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './counterSlice';
-
+import cartReducer from '../store/cartSlice';
+import todoReducer from '../store/todoSlice';
 
 export const store = configureStore({
-    reducer:{
-        counter: counterReducer,
-    },
+  reducer: {
+    cart: cartReducer,
+    todo: todoReducer,
+  },
 });
 
-
 export type RootState = ReturnType<typeof store.getState>;
-
-
 export type AppDispatch = typeof store.dispatch;
